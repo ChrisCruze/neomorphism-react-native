1d662193ed2200fc0b7d5fe2dde50b5f0b3d3949
ChrisCruze <cruzc09@gmail.com>
2020-02-22 00:54:03 -0500

    first attempt at creating neomorphism

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          height: 100
+          // padding: 20
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "#E0E5EC",
+            shadowColor: "#a3b1c6",
+            shadowRadius: "16px",
+            shadowOpacity: 0.6,
+            shadowOffset: { width: "9px", height: "9px" },
+            flex: 0.5
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "#E0E5EC",
+              shadowColor: "#ffffff",
+              shadowRadius: "16px",
+              shadowOpacity: 0.5,
+              shadowOffset: { width: "-9px", height: "-9px" },
+              flex: 1
+            }}
+          ></View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -12,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#E0E5EC",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
